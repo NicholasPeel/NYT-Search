06401d83971a9e7b0a2a8c443c71c0458a4127ae
Nick Peel <nickpeel@Nicks-MacBook-Pro.local>
2018-06-24 17:34:16 -0700

    ffff

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ if (process.env.NODE_ENV === "production") {
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-app.listen(PORT, function() {
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/nytReact");
+
+app.listen(PORT, function () {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
 });
